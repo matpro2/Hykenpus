8c5ea9514e18f9c361501f04b6b5c4163499c68b
matpro2 <165037110+matpro2@users.noreply.github.com>
2026-03-11 15:20:59 +0000

    flemme de mettre des nom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { saeService } from './services/saeService';
+import { saeService } from './services/saeServices';
 import './App.css';
 
 function App() {
