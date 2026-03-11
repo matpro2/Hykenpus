902b93f7a595a678e18ab1c5911952591262068d
matpro2 <165037110+matpro2@users.noreply.github.com>
2026-03-11 15:33:00 +0100

    Update API_URL to new GitHub Pages URL

--- frontend/services/saeService.js
+++ frontend/services/saeService.js
@@ -1,7 +1,7 @@
 // frontend/src/services/saeService.js
 
 // N'oublie pas de remplacer par l'URL de ton port 8000 rendu public !
-const API_URL = 'https://TON_URL_CODESPACES_ICI/api/sae'; 
+const API_URL = 'https://didactic-fortnight-r47xp459jq9535477-5173.app.github.dev/api/sae'; 
 
 export const saeService = {
   getListeSae: async () => {
